98fd91ba6ed0e6cbd001beacbe11b98ad487c755
VladAntR <88376848+VladAntR@users.noreply.github.com>
2022-03-22 14:16:55 +0200

    Update project

--- gulp/task/server.js
+++
@@ -1,9 +0,0 @@
-export const server = (done) =>{
-    app.plugins.browser_sync.init({
-        server: {
-            baseDir: `${app.path.build.html}`
-        },
-        notify: false,
-        port: 3000,
-    });
-}
